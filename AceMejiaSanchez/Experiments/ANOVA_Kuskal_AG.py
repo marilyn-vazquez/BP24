e2883a6a9d8f6388ea095553af6fe05245ccadc8
Ace Mejia-Sanchez <60498918+PrincipitoAce@users.noreply.github.com>
2024-07-09 10:13:36 -0500

    Applied changes to Measure_Patterns, edited WVS_MeasurePatterns, & created ANOVA_Kuskal_AG

---
+++ AceMejiaSanchez/Experiments/ANOVA_Kuskal_AG.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  9 09:53:52 2024
+
+@author: aceme
+"""
+################## Import relevant packages
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import numpy as np
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+from sklearn import svm
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import SVC
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+import random
+from scipy.stats import f_oneway
+from itertools import combinations
+################## Import function ######################################################
+import sys
+sys.path.append('C:/Users/aceme/OneDrive/Documents/GitHub/BP24/')
+import Measure_Patterns
+
+# ################## ANOVA FvL Re-Write ####################################
+
+# # Import data
+# df = pd.read_csv("C:/Users/aceme/OneDrive/Documents/GitHub/BP24/Ellee/Sanity Checks/Demos/stacked_all.csv")
+
+# # Indexing through pre-prepared splitting in stacked_all
+# X_train = df.iloc[:168, :16]
+# # X_test = df.iloc[168:241, :16]
+# # y_train =  df.iloc[:168, 22]
+# # y_test = df.iloc[168:241, 22]
+
+# print("\n------------------ ANOVA (Feature vs Label) -----------------------")
+
+# # Finds dependency between all features in X_train & the label in y_train
+# def anova_fvl(X_train, y_train):
+    
+#     # Combining X_train and y_train
+#     df = X_train
+#     df['y_train'] = y_train
+
+#     # Number of features, excluding label
+#     var_count = len(X_train.columns)-1
+
+#     # Creates an empty array for f-statistic and P-values
+#     results = []
+
+#     for i in range(0, var_count):
+        
+#         # Compute ANOVA
+#         f_statistic, p_value = f_oneway(df.iloc[:,i], df.iloc[:,-1])
+        
+#         # Save p-value significance into list
+#         if p_value < 0.05:
+#             significance = "Significant"
+#         else:
+#             significance = "Not Significant"
+           
+#         # Append results to the list
+#         results.append({
+#             "Feature": df.columns[i],
+#             "F-Statistic": f_statistic,
+#             "P-Value": p_value, 
+#             "Significance": significance})
+
+#     # Create a dataFrame from the results
+#     results_df = pd.DataFrame(results)
+    
+#     # Print the dataFrame
+#     print("Label:", df.columns.values[-1])
+#     print(results_df.to_string(index=False))
+
+# # Testing consistency of ANOVA test
+# for i in range(16, 25, 1):
+#     # Loop through Poisson-distributed categorical features in stacked_all
+#     y_train = df.iloc[:168, i]
+    
+#     # Run ANOVA
+#     anova_fvl(X_train, y_train)
+    
+################## Graphing ####################################
+
+# Data
+categories = ['ANOVA', 'Kuskal-Wallis H Test', 'Anderson-Govern']
+sigs = [10, 20, 30]
+no_sigs = [15, 25, 35]
+
+# Number of categories
+n = len(categories)
+
+# X axis locations for the groups
+ind = np.arange(n)
+
+# Width of the bars
+width = 0.35
+
+# Plotting
+fig, ax = plt.subplots()
+
+# Bars for Significant
+bar1 = ax.bar(ind - width/2, sigs, width, label='Significant')
+
+# Bars for Not Significant
+bar2 = ax.bar(ind + width/2, no_sigs, width, label='Not Significant')
+
+# Adding labels, title, and legend
+ax.set_xlabel('Tests')
+ax.set_ylabel('Values')
+ax.set_title('Side by Side Bar Chart for Tests')
+ax.set_xticks(ind)
+ax.set_xticklabels(categories)
+ax.legend()
+
+# Show plot
+plt.show()
