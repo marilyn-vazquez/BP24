18cf7c04f1088aa9c6c10d734343fd63f5590d53
Ace Mejia-Sanchez <60498918+PrincipitoAce@users.noreply.github.com>
2024-07-17 09:02:59 -0500

    Updated WVS_Measure_Patterns.py so that the WVS dataset keeps its original floats and ints

--- AceMejiaSanchez/Experiments/WVS_MeasurePatterns.py
+++ AceMejiaSanchez/Experiments/WVS_MeasurePatterns.py
@@ -56,6 +56,16 @@ label_index = df.columns.get_loc('Q46')
 old_q1 = df.Q1.unique()
 old_q46 = df.Q46.unique()
 
+########### Keep track of column types bc mapping with np.nan will reduce to all floats
+# Identify integer columns and store the list of columns
+int_columns = [col for col in df.columns if pd.api.types.is_integer_dtype(df[col])]
+# Some columns are eliminated later in the code, so they are being removed from this list
+cols_to_remove = ['Q82_AFRICANUNION', 'Q82_APEC', 'Q82_ARABLEAGUE', 'Q82_ASEAN', 'Q82_CIS', 'Q82_CUSMA', 'Q82_ECO', 'Q82_EU', 'Q82_GULFCOOP', 'Q82_ISLCOOP', 'Q82_MERCOSUR', 'Q82_NAFTA', 'Q82_OAS', 'Q82_SAARC', 'Q82_SCO', 'Q82_TLC', 'Q82_UNDP', 'Q276', 'Q276A', 'Q276R', 'Q280', 'Q282', 'Q284', 'Q291G1', 'Q291G2', 'Q291G3', 'Q291G4', 'Q291G5', 'Q291G6', 'Q291P1', 'Q291P2', 'Q291P3', 'Q291P4', 'Q291P5', 'Q291P6', 'Q291UN1', 'Q291UN2', 'Q291UN3', 'Q291UN4', 'Q291UN5', 'Q291UN6', 'Q292A', 'Q292B', 'Q292C', 'Q292D', 'Q292E', 'Q292F', 'Q292G', 'Q292H', 'Q292I', 'Q292J', 'Q292K', 'Q292L', 'Q292M', 'Q292N', 'Q292O', 'Q293', 'Q294A', 'Q294B']
+integer_columns = [item for item in int_columns if item not in cols_to_remove]
+
+# Identify object columns and store the list of columns
+object_columns = [col for col in df.columns if pd.api.types.is_object_dtype(df[col])]
+
 # Define the mapping for missing values
 value_mapping = {
     -1: np.nan,
@@ -147,12 +157,14 @@ df_shape = df.shape
 print("Old Shape:", old_shape)
 print("New Shape:", new_shape)
 
-print(df.dtypes)   
+### Convert the stored list of columns back to integer & object types
+df[integer_columns] = df[integer_columns].astype(int)
+df[object_columns] = df[object_columns].astype('object')
 
 ################# Split dataset into X_train and y_train ####################################
 X_train, X_test, y_train, y_test = train_test_split(df.iloc[:,start_col_index:-1].drop(columns=['Q46']), df.iloc[:,label_index], test_size=0.2, random_state=42)
 
 ################# Running Measure_Patterns() ####################################
-#Fake_Patterns.FakePatterns(X_train, y_train)
+Fake_Patterns.FakePatterns(X_train, y_train)
 
 
