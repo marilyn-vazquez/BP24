d24f4caaad3634d275024b10a975a707efcc87ef
katehuisinga <kate.huisinga@my.simpson.edu>
2024-07-17 11:14:31 -0500

    moved demo

--- Demo_kate.py
+++ Demo_kate.py
@@ -1,13 +1,18 @@
 ################## Import function ############################################
 import sys
 import pandas as pd
-sys.path.append("C:/Users/kateh/OneDrive/Documents/GitHub/BP24/Fake_Patterns.py")
+import numpy as np
+sys.path.append("C:/Users/kateh/OneDrive/Documents/GitHub/BP24/Fake_Differences.py")
 import Fake_Differences
 
 ################## Import data ################################################
 df1 = pd.read_csv("C:/Users/kateh/OneDrive/Documents/GitHub/BP24/Ellee/Data/Gaussian/gaussian_orig.csv", header=None)
-df2 = pd.read_csv("C:/Users/kateh/OneDrive/Documents/GitHub/BP24/AugSynDatasets/gaussian_randswap.csv", header=None)
-
+df_aug = pd.read_csv("C:/Users/kateh/OneDrive/Documents/GitHub/BP24/AugSynDatasets/gaussian_randswap.csv", header=None)
+df_test = pd.read_csv("C:/Users/kateh/OneDrive/Documents/GitHub/BP24/AugSynDatasets/gaussian_test.csv", header=None)
+array1 = df_aug.to_numpy()
+array2 = df_test.to_numpy()
+array = np.vstack((array1, array2))
+df2 = pd.DataFrame(array)
 ################## Data cleaning ##############################################
 # Whatever changes you make to your data set
 
